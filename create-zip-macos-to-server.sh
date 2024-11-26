dda13bd71c743ca00e710fca14522bad282a320a
ShaneBeee <shanebolenback@me.com>
2024-11-26 15:01:31 -0800

    create-zip-macos-to-server.sh - we dont need to make a directory

--- create-zip-macos-to-server.sh
+++ create-zip-macos-to-server.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/src/main/resources"
-mkdir -p "../../../output/"
 exec zip -r /Users/ShaneBee/Desktop/Server/Skript/1-21-2/worlds/world/datapacks/Beer.zip . -x '**/.DS_Store'
 exit
