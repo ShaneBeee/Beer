1f237c822206f4d40cd54f96587643b85002a5ab
ShaneBeee <shanebolenback@me.com>
2024-12-10 13:43:09 -0800

    version updates

--- create-zip-macos-to-server.sh
+++ create-zip-macos-to-server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/src/main/resources"
-exec zip -r /Users/ShaneBee/Desktop/Server/Skript/1-21-2/worlds/world/datapacks/Beer.zip . -x '**/.DS_Store'
+exec zip -r /Users/ShaneBee/Desktop/Server/Skript/1-21-4/worlds/world/datapacks/Beer.zip . -x '**/.DS_Store'
 exit
