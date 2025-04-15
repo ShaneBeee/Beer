84edcc258ec058476f5e4dc12c5e7a8df02ee75e
ShaneBeee <shanebolenback@me.com>
2025-04-15 08:37:59 -0700

    build.gradle.kts - update paperweight

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.userdev.ReobfArtifactConfiguration
 plugins {
     id("java")
     id("io.github.goooler.shadow") version "8.1.7"
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.8" // the latest version can be found on the Gradle Plugin Portal
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16" // the latest version can be found on the Gradle Plugin Portal
 }
 
 // The Minecraft version we're currently building for
