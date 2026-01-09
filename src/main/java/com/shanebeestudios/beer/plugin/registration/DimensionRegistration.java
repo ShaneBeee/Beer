bd23f2a5507b583c3d48517a2ed333728460e0d8
ShaneBeee <shanebolenback@me.com>
2026-01-08 16:13:54 -0800

    DimensionRegistration - fix weirdness scale

--- src/main/java/com/shanebeestudios/beer/plugin/registration/DimensionRegistration.java
+++ src/main/java/com/shanebeestudios/beer/plugin/registration/DimensionRegistration.java
@@ -13,7 +13,7 @@ import com.shanebeestudios.beer.plugin.biomes.special.CaveBiomes;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
-import net.minecraft.world.level.biome.Climate;
+import net.minecraft.world.level.biome.Climate.Parameter;
 import org.jetbrains.annotations.NotNull;
 
 @SuppressWarnings("SameParameterValue")
@@ -25,47 +25,49 @@ public class DimensionRegistration {
         DimensionDefinition.Builder builder = DimensionDefinition.overworldBuilder(Dimensions.BEER_WORLD);
 
         // CAVE BIOMES
-        builder.addPoint(Biomes.DRIPSTONE_CAVES, Climate.Parameter.span(0.8f, 1.0f),
+        builder.addPoint(Biomes.DRIPSTONE_CAVES, Parameter.span(0.8f, 1.0f),
             BiomeDefaults.FULL_RANGE, BiomeDefaults.FULL_RANGE, BiomeDefaults.FULL_RANGE,
-            Climate.Parameter.span(0.2f, 0.9f), BiomeDefaults.FULL_RANGE, 0);
+            Parameter.span(0.2f, 0.9f), BiomeDefaults.FULL_RANGE, 0);
 
         builder.addPoint(Biomes.DEEP_DARK, BiomeDefaults.FULL_RANGE,
-            BiomeDefaults.FULL_RANGE, BiomeDefaults.FULL_RANGE, Climate.Parameter.span(-1.0f, -0.375f),
-            Climate.Parameter.span(1.1f, 2.0f), BiomeDefaults.FULL_RANGE, 0);
+            BiomeDefaults.FULL_RANGE, BiomeDefaults.FULL_RANGE, Parameter.span(-1.0f, -0.375f),
+            Parameter.span(1.1f, 2.0f), BiomeDefaults.FULL_RANGE, 0);
 
         for (int tempIndex = 0; tempIndex < BiomeDefaults.TEMPERATURES.length; tempIndex++) {
-            Climate.Parameter temp = BiomeDefaults.TEMPERATURES[tempIndex];
+            Parameter temp = BiomeDefaults.TEMPERATURES[tempIndex];
             for (int humidityIndex = 0; humidityIndex < BiomeDefaults.HUMIDITIES.length; humidityIndex++) {
-                Climate.Parameter humidity = BiomeDefaults.HUMIDITIES[humidityIndex];
+                Parameter humidity = BiomeDefaults.HUMIDITIES[humidityIndex];
 
                 ResourceKey<Biome> biomeKey = CaveBiomes.getBiome(tempIndex, humidityIndex);
                 if (biomeKey == null) continue;
 
                 builder.addPoint(biomeKey, BiomeDefaults.FULL_RANGE, temp, humidity, BiomeDefaults.FULL_RANGE,
-                    Climate.Parameter.span(0.078125f, 0.9f), BiomeDefaults.FULL_RANGE, 0);
+                    Parameter.span(0.078125f, 0.9f), BiomeDefaults.FULL_RANGE, 0);
             }
         }
 
         // OVERWORLD BIOMES
         for (int contIndex = 0; contIndex < BiomeDefaults.CONTINENTALNESS.length; contIndex++) {
-            Climate.Parameter continentalness = BiomeDefaults.CONTINENTALNESS[contIndex];
+            Parameter continentalness = BiomeDefaults.CONTINENTALNESS[contIndex];
 
             for (int tempIndex = 0; tempIndex < BiomeDefaults.TEMPERATURES.length; tempIndex++) {
-                Climate.Parameter temp = BiomeDefaults.TEMPERATURES[tempIndex];
+                Parameter temp = BiomeDefaults.TEMPERATURES[tempIndex];
 
                 for (int humidityIndex = 0; humidityIndex < BiomeDefaults.HUMIDITIES.length; humidityIndex++) {
-                    Climate.Parameter humidity = BiomeDefaults.HUMIDITIES[humidityIndex];
+                    Parameter humidity = BiomeDefaults.HUMIDITIES[humidityIndex];
 
                     for (int erosionIndex = 0; erosionIndex < BiomeDefaults.EROSIONS.length; erosionIndex++) {
-                        Climate.Parameter erosion = BiomeDefaults.EROSIONS[erosionIndex];
+                        Parameter erosion = BiomeDefaults.EROSIONS[erosionIndex];
 
                         for (int pvIndex = 0; pvIndex < BiomeDefaults.PVS.length; pvIndex++) {
 
                             for (int weirdIndex = 0; weirdIndex < BiomeDefaults.PVS[pvIndex].length; weirdIndex++) {
-                                Climate.Parameter weirdness = BiomeDefaults.PVS[pvIndex][weirdIndex];
+                                Parameter weirdness = BiomeDefaults.PVS[pvIndex][weirdIndex];
+
+                                int weirdVal = weirdness.max() <= 0 ? 0 : 1;
 
                                 ResourceKey<Biome> biomeKey = getBiome(contIndex, tempIndex, humidityIndex,
-                                    weirdIndex, pvIndex, erosionIndex);
+                                    weirdVal, pvIndex, erosionIndex);
 
                                 builder.addPoint(biomeKey, continentalness, temp, humidity, erosion,
                                     BiomeDefaults.FULL_RANGE, weirdness, 0);
